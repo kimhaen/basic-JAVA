502c7a52365c4a1dd9aaa77b1dbc33d10839fb2f
kimhaen <50911139+kimhaen@users.noreply.github.com>
2019-10-31 14:11:22 +0900

    Create exam_03.java

---
+++ exam_03.java
@@ -0,0 +1,35 @@
+package test;
+
+public class exam_01 {
+	public static void main (String[] args) {
+		
+		System.out.println("Hello, world!");
+		System.out.println("================="); // 変数名を指定、数字を入力、出力
+		
+		int year = 2019;
+		int age = 27;
+		
+		System.out.println(year);
+		System.out.println(age);
+		
+		year = year + 1;
+		age = age + 1;
+		
+		System.out.println("=================");
+		System.out.println(year);
+		System.out.println(age);
+		
+		System.out.println("================="); //　変数の数字をTmp変数を使って入れ代わります。
+		int x = 10, y = 20;
+		int tmp = 0;
+		
+		System.out.println("x:" + x + " y:" + y);
+		
+		tmp = x;
+		x = y;
+		y = tmp;
+		
+		System.out.println("x:" + x + " y:" + y);
+		
+	}
+}
